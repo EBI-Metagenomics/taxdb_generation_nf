9fa42ae83171ae4bedc17fa90f12f59f1652f691
Sofia Ochkalova <so.ochkalova@gmail.com>
2024-11-06 12:41:45 +0000

    modify to correctly capture ENZYME field

--- bin/reformat_rhea_chebi_mapping.py
+++ bin/reformat_rhea_chebi_mapping.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import argparse
 import gzip
 import re
@@ -11,14 +13,14 @@ def convert_to_tsv(gz_file_path: Path) -> Path:
     with gzip.open(gz_file_path, "rt") as file_in, open(output_file_path, "w") as file_out:
         input_text = file_in.read()
         pattern = re.compile(
-            r"ENTRY\s+(.*?)\nDEFINITION\s+(.*?)\nEQUATION\s+(.*?)\n(?:ENZYME\s+(.*?))?\n*",
-            re.DOTALL,
+            r"ENTRY\s+(\S+)\nDEFINITION\s+(.+?)\nEQUATION\s+(.+?)(?:\nENZYME\s+(.+?))?\n///",
+            re.DOTALL
         )
         matches = pattern.findall(input_text)
         file_out.write("ENTRY\tDEFINITION\tEQUATION\tENZYME\n")
         for match in matches:
             entry, definition, equation, enzyme = match
-            enzyme = enzyme.strip() if enzyme else ""
+            enzyme = " ".join(enzyme.strip().split()) if enzyme else ""
             file_out.write(f"{entry}\t{definition}\t{equation}\t{enzyme}\n")
 
     # Clean up the downloaded .gz file
