6b6be480106910d250dee13bddac1c85d1f6e9e4
Sofia Ochkalova <so.ochkalova@gmail.com>
2024-11-11 15:57:39 +0000

    remove comments

--- bin/reformat_rhea_chebi_mapping.py
+++ bin/reformat_rhea_chebi_mapping.py
@@ -9,7 +9,6 @@ def convert_to_tsv(gz_file_path: Path) -> Path:
 
     output_file_path = "rhea_chebi_mapping.tsv"
 
-    # Process the .gz file and convert it to TSV
     with gzip.open(gz_file_path, "rt") as file_in, open(output_file_path, "w") as file_out:
         input_text = file_in.read()
         pattern = re.compile(
@@ -23,7 +22,6 @@ def convert_to_tsv(gz_file_path: Path) -> Path:
             enzyme = " ".join(enzyme.strip().split()) if enzyme else ""
             file_out.write(f"{entry}\t{definition}\t{equation}\t{enzyme}\n")
 
-    # Clean up the downloaded .gz file
     gz_file_path.unlink()
     return output_file_path
 
